a7814f2ffc5ff9be7e9362c02da9a21db53a3bc7
Yuthirshta Prasad T <yuthish661@gmail.com>
2022-06-04 21:36:55 +0530

    upd -> nan rename

--- flask-backend/Excel_Processor/processing.py
+++ flask-backend/Excel_Processor/processing.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
 dataset = pd.read_csv("./Excel_Processor/test_error.csv")
-def NaN_Finder(dataset):
+def NaN_Checker(dataset):
   row = 0
   col = 0
-  L = []
+  nan_error = []
   for i in range(len(dataset)):
     for j in range(23):
       if(str(dataset.iloc[i,j])=="nan"):
         row = i
         col = j
-        L.append(f'The row is {row+1} and col is {col+1} \n')
-  print(L)
+        nan_error.append(f'The row is {row+1} and col is {col+1} \n')
+  print(nan_error)
 
 
-NaN_Finder(dataset)
+NaN_Checker(dataset)
