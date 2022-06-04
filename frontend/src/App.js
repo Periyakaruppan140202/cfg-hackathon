e24b9a1afec6b5ceaf01e54cf03da04386a4e3e5
Periyakaruppan <periyakaruppannagappan2020@gmail.com>
2022-06-04 18:45:49 +0530

    Resolved pull issue

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
