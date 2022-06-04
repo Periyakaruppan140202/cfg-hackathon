f849d64b555c230c4279429e9eab41e24dda8c99
Prerna Pahwa <prernapahwa64@gmail.com>
2022-06-05 01:02:15 +0530

    bar chart for attendence

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Excel from "./pages/Excel/Excel";
-import MainHome from "./pages/MainHome/MainHome";
+import Chart2 from "./pages/Chart/chart2";
+
 import Chart from "./pages/Chart/chart";
 
 function App() {
@@ -14,9 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path= "/excel" element= {<Excel/>} />
-        <Route path= "/MainHome" element= {<MainHome/>} />
         <Route path= "/chart" element= {<Chart/>} />
-        
+        <Route path= "/chart2" element= {<Chart2/>} />
       </Routes>
     </BrowserRouter>
   );
