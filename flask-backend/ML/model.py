0cc21d8cc9c5aa49917fd30239538060c97d1f27
pra17dod <pra17dod@gmail.com>
2022-06-04 17:11:13 +0530

    ML: add model.py

---
+++ flask-backend/ML/model.py
@@ -0,0 +1,19 @@
+import pickle
+import numpy as np
+# import
+
+def predict(X):
+    X = np.array(X) # X is 2D array
+    kmeans_model = pickle.load(open("model.pkl", "rb"))
+    y_kmeans = kmeans_model.fit_predict(X)
+    c1 = [X[y_kmeans == 0, 0],X[y_kmeans == 0, 1]] # cluster 1 co-ordinates
+    c2 = [X[y_kmeans == 1, 0],X[y_kmeans == 1, 1]] # cluster 2 co-ordinates
+
+    return c1,c2
+
+if __name__ == "__main__":
+    # print(predict(sys.argv[1])
+    c1,c2 = predict([[23,2],[22,4],[26,0],[20,8],[88,4],[18,8],[14,0],[88,4],[22,0],[80,2]])
+    print(c1)
+    print(c2)
+    
--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 
+from ML.model import predict
+
 app = Flask(__name__)
 CORS(app)
 
@@ -10,6 +12,16 @@ def home():
     print("Server is up and running")
     return jsonify({msg : "Hello Flask Server"}),200
 
+
+@app.route('/cluster', methods=['POST'])
+def cluster():
+    data = request.get_json(force=True)
+    X = data['array'] #[[1,2],[3,4],...]
+    c1,c2 = predict(X)
+    out = {'cluster1': c1,'cluster2':c2}
+    return jsonify(out),200
+
+
 if(__name__) == "__main__":
     app.run(debug=True)
-    
+
