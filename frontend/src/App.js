e17767b4628c7104875adffff3e61ae46a241411
Sara Sameer Sheth <sarasheth60@gmail.com>
2022-06-05 03:54:45 +0530

    prepared charts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,10 @@ import Home from "./pages/Home/Home";
 import Excel from "./pages/Excel/Excel";
 import Chart2 from "./pages/Chart/chart2";
 import Chart4 from "./pages/Chart/chart4";
+import Chart3 from "./pages/Chart/chart3"
 import Chart from "./pages/Chart/chart";
 import Form from "./pages/Form/Form"
+
 function App() {
   return (
     <BrowserRouter>
@@ -17,6 +19,7 @@ function App() {
         <Route path= "/excel" element= {<Excel/>} />
         <Route path= "/chart" element= {<Chart/>} />
         <Route path= "/chart2" element= {<Chart2/>} />
+        <Route path= "/chart3" element= {<Chart3/>} />
         <Route path="/form" element = {<Form/>}/>
         <Route path= "/chart4" element= {<Chart4/>} />
       </Routes>
