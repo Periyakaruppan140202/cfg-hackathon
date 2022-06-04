a2ad7dffa6b4b02d9091d71028b498203a43a19c
Yuthirshta Prasad T <yuthish661@gmail.com>
2022-06-04 18:22:26 +0530

    fix -> cluster dependency

--- flask-backend/ML/model.py
+++ flask-backend/ML/model.py
@@ -1,14 +1,12 @@
 import pickle
 import numpy as np
-# import
-
+import sklearn.cluster
 def predict(X):
     X = np.array(X) # X is 2D array
     kmeans_model = pickle.load(open("model.pkl", "rb"))
-    y_kmeans = kmeans_model.fit_predict(X)
+    y_kmeans = kmeans_model.predict(X)
     c1 = [X[y_kmeans == 0, 0],X[y_kmeans == 0, 1]] # cluster 1 co-ordinates
     c2 = [X[y_kmeans == 1, 0],X[y_kmeans == 1, 1]] # cluster 2 co-ordinates
-
     return c1,c2
 
 if __name__ == "__main__":
