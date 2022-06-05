d7ed5adcf3ed74317be66a9364cdf8bbfb8f733f
Chiragjain55551 <chiragjain99991@gmail.com>
2022-06-05 06:06:31 +0530

    cards for providing information about solutions on landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,14 +13,14 @@ import Chart4 from "./pages/Chart/chart4";
 import Chart3 from "./pages/Chart/chart3";
 import Form from "./pages/Form/Form";
 import FellowAnalysis from "./pages/FellowAnalysis/Analysis";
-import MainHome from "./pages/MainHome/MainHome";
+// import MainHome from "./pages/MainHome/MainHome";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mainhome" element={<MainHome />} />
+        {/* <Route path="/mainhome" element={<MainHome />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/excel" element={<Excel />} />
         {/* <Route path="/MainHome" element={<MainHome />} /> */}
